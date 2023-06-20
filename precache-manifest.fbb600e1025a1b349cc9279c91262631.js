0437ec8866acc673815b8d7fe2f0389583fe8924
Urvashi Indu <urvashiindu09@gmail.com>
2023-06-20 23:29:45 +0530

    Updates

---
+++ precache-manifest.fbb600e1025a1b349cc9279c91262631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edf2b580916999f8d715",
+    "url": "/Word_Counter_using_React/static/css/main.8b1a211c.chunk.css"
+  },
+  {
+    "revision": "edf2b580916999f8d715",
+    "url": "/Word_Counter_using_React/static/js/main.edf2b580.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Word_Counter_using_React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "cddc92c48fb4db0237a8",
+    "url": "/Word_Counter_using_React/static/js/2.cddc92c4.chunk.js"
+  },
+  {
+    "revision": "56240808dbf95f5cd939",
+    "url": "/Word_Counter_using_React/static/js/runtime~main.56240808.js"
+  },
+  {
+    "revision": "1dd2b228aca2435712315a654b171b44",
+    "url": "/Word_Counter_using_React/index.html"
+  }
+];
